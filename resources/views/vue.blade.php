d7ae86852c60173f571faeb9db3a7364115b1c43
sutin1234 <tony.sutin1234@gmail.com>
2019-01-20 14:56:52 +0700

    add readme.md

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel + Vue server side rendering example</title>
+        <title>Laravel + Vue server side rendering</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script defer src="{{ mix('js/vue/entry-client.js') }}"></script>
     </head>
